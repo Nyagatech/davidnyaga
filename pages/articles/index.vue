21e96b449c814bd7d94a620c3d22d8c2755be0f8
David Nyaga <105038549+Nyagatech@users.noreply.github.com>
2025-02-18 23:45:27 +0300

    addded the articles page

---
+++ pages/articles/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container mx-auto border-x border-gray-900 bg-slate-950">
+        <div class="flex justify-center">
+      <Navbar />
+    </div>
+    <div class="mt-20">
+      <Image class="mx-4" />
+    </div>
+    <div class="mt-20 text-center">
+        <P class="text-xl font-bold">Working on it...</P>
+    </div>
+
+    </div>
+</template>
